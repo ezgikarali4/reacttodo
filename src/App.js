dab66b3467e18aee717d40fbf4b44638a7532f4b
ezgikarali <ezgi.karali505@gmail.com>
2022-09-20 13:12:11 +0300

    todo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Components from "./Components"; // Components'i gösterebilmek için import ediyoruz.
 
 function App() {
   return (
     <div className="App">
-      
+      <Components />
     </div>
   );
 }
